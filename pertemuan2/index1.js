b0dcce3b0ccfd41092fdf3465c3532bc41df56b4
armansyabani <armansyabni@gmail.com>
2025-10-31 16:37:58 +0700

    membuat variabel dan tipe data

---
+++ pertemuan2/index1.js
@@ -0,0 +1,20 @@
+// membuat variabel dan tipe datanya
+let nama = "arman";
+const umur = 19;
+var alamat = "tasikmalaya";
+
+// membuat oprator  aritmatika dan perbandingan untuk membandingkan dua angka.
+let angka1 = 10;
+let angka2 = 20;
+let penjumlahan = angka1 + angka2;
+let pengurangan = angka2 - angka1;
+let perkalian = angka1 * angka2;
+let pembagian = angka2 / angka1;
+let sisaBagi = angka2 % angka1;
+let pangkat = angka1 ** 2;
+console.log("Hasil Penjumlahan: " + penjumlahan);
+console.log("Hasil Pengurangan: " + pengurangan);
+console.log("Hasil Perkalian: " + perkalian);
+console.log("Hasil Pembagian: " + pembagian);
+console.log("Hasil Sisa Bagi: " + sisaBagi);
+console.log("Hasil Pangkat: " + pangkat);
