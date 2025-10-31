6da8dc1a620b11adc075636f7f358e299bffe202
armansyabani <armansyabni@gmail.com>
2025-10-31 17:09:20 +0700

    membuat variabel dan tipe data

--- pertemuan2/index1.js
+++ pertemuan2/index1.js
@@ -1,20 +1,39 @@
-// membuat variabel dan tipe datanya
-let nama = "arman";
-const umur = 19;
-var alamat = "tasikmalaya";
 
-// membuat oprator  aritmatika dan perbandingan untuk membandingkan dua angka.
-let angka1 = 10;
-let angka2 = 20;
-let penjumlahan = angka1 + angka2;
-let pengurangan = angka2 - angka1;
-let perkalian = angka1 * angka2;
-let pembagian = angka2 / angka1;
-let sisaBagi = angka2 % angka1;
-let pangkat = angka1 ** 2;
-console.log("Hasil Penjumlahan: " + penjumlahan);
-console.log("Hasil Pengurangan: " + pengurangan);
-console.log("Hasil Perkalian: " + perkalian);
-console.log("Hasil Pembagian: " + pembagian);
-console.log("Hasil Sisa Bagi: " + sisaBagi);
-console.log("Hasil Pangkat: " + pangkat);
+
+const string1 = "contoh string 1" // string
+const integer1 = 76 // integer
+const boolean1 = true; // bloolean
+const null1 = null // null
+const undifined1 = undefined; // undifined
+
+var string2 = "contoh string 2" // string
+var integer2 = 76 // integer
+var boolean2 = true; // bloolean
+var null2 = null // null
+var undifined2 = undefined; // undifined
+
+let a = 2
+let b = 3
+console.log("Hasil Penjumlahan: " + (a + b));
+console.log("Hasil Pengurangan: " + (a - b));
+console.log("Hasil Perkalian: " + (a * b));
+console.log("Hasil Pembagian: " + (a / b));
+console.log("Hasil Modulus: " + (a % b));
+console.log("Hasil Exponen: " + (a ** b));
+
+let c = 10;
+let d = 15;
+console.log (a == b) //equal
+console.log (a != b) //not equal
+console.log (a > b) //greater than
+console.log (a < b) //less than
+console.log (a >= b) //greater than equal
+console.log (a <= b) //less than equal
+console.log (a === b) //strict equal
+console.log (a !== b) //strict not equal
+
+let e = 20;
+let f = 25;
+console.log ( (a < b) && (e < f) ) //and
+console.log ( (a < b) || (e < f) ) //or
+console.log ( !(a < b) ) //not
---
+++ pertemuan2/index2.js
@@ -0,0 +1,12 @@
+//data produk
+let produk1 = "sabun mandi";
+let produk2 = "shampo zink";
+
+//data harga produk
+let hargaProduk1 = 7000;
+let hargaProduk2 = 5000;
+
+//jumlah total semua produk
+let totalHarga = hargaProduk1 + hargaProduk2;
+console.log("Total Harga Produk: " + totalHarga);
+total harga sabun muka dan sampo adalah 12000
