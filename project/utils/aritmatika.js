5b0b98da15e7d5d1446bc6a63e3a2d3c7dedaaf5
armansyabani <armansyabni@gmail.com>
2025-11-13 11:01:17 +0700

    feat: :fire:

---
+++ project/utils/aritmatika.js
@@ -0,0 +1,19 @@
+// fungsi menambah
+function tambah(a, b) {
+    return a + b;
+}
+
+// fungsi mengurangi
+function kurang(a, b) {
+    return a - b;
+}
+
+// fungsi perkalian
+function kali(a, b) {
+    return a * b;
+}
+
+// fungsi pembagian
+function bagi(a, b) {
+    return a / b;
+}// harus a / b kalau nol akan error
